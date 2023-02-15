6890bb0f02fbeca000112f2542452a6e3379af77
kevinville <kevinlhardyal@gmail.com>
2023-02-14 19:50:03 -0500

    Added Activity 5 Code

---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace WebAPIClient
+{
+    class Switchgames
+    {
+        [JsonProperty("anime")]
+        public string animu { get; set; }
+        [JsonProperty("character")]
+        public string character { get; set; }
+
+        [JsonProperty("quote")]
+        public string quote { get; set; }
+        
+    }
+
+    class Program
+    {
+        private static readonly HttpClient client = new HttpClient();
+
+
+        static async Task Main(string[] args)
+        {
+            await ProcessRepositories();
+        }
+        private static async Task ProcessRepositories()
+        {
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Enter Switch Game, enter without writing a name to quit.");
+
+                    var switchGame = Console.ReadLine();
+
+                    if (string.IsNullOrEmpty(switchGame))
+                    {
+                        break;
+                    }
+
+                    var result = await client.GetAsync("https://animechan.vercel.app/api/random/anime?title=" + switchGame);
+                    var resultRead = await result.Content.ReadAsStringAsync();
+
+                    var switchstuff = JsonConvert.DeserializeObject<Switchgames>(resultRead);
+
+                    Console.WriteLine("---");
+                    Console.WriteLine("Anime: " + switchstuff.animu);
+                    Console.WriteLine("Character Name: " + switchstuff.character);
+                    Console.WriteLine("Quote: " + switchstuff.quote);
+
+                    Console.WriteLine("\n---");
+                }
+                catch(Exception)
+                {
+                    Console.WriteLine("Invalid Switch Game.");
+                }
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # IntroToAPIActivity5
-hello
+
+
+Try the API with requests from 3 different popular animes like
+
+- Naruto
+- One Piece
+- Bleach
+
+Made using the api from https://animechan.vercel.app
